8577d80ca642f2aca26daa6127427c446dba8c38
WILLIER GONCALVES <CG3038203@car.ifsp>
2026-07-31 10:29:33 -0300

    31/07v2

--- Cenas/area_2d_2.gd
+++ Cenas/area_2d_2.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
-		var alvo1 = get_parent().get_node("Area2D")
+		var alvo1 = get_parent().get_node("Marker2D")
 
 		for barco in get_tree().get_nodes_in_group("barcos"):
 			barco.definir_alvo(alvo1)
--- barcos.gd
+++ barcos.gd
@@ -6,7 +6,9 @@ extends Node2D
 @onready var timer10s = $Timer
 
 var alvo: Node2D = null
+
 var seguindo = false
+var parado_no_alvo = false
 
 var capacidade = 10
 var estoque = 0
@@ -21,67 +23,102 @@ var speedy = 200
 var direction = 1
 
 var limite = 150
-var inicio_x
+var inicio_x = 0
+
+
+func _ready():
+	add_to_group("barcos")
+
 
 func solto():
 	caixas.tiracaixa()
 	timer10s.start()
 	soltou = true
+	andar()
+
 
 func andar():
 	inicio_x = position.x
 
+
 func definir_alvo(novo_alvo: Node2D):
 	alvo = novo_alvo
 	seguindo = true
+	parado_no_alvo = false
+
 
 func parar_de_seguir():
 	alvo = null
 	seguindo = false
 
-func _ready():
-	add_to_group("barcos")
+
+func seguir_alvo(delta):
+	if alvo == null:
+		return
+
+	if global_position.x < alvo.global_position.x:
+		global_position.x += speed * delta
+		sprite.flip_h = false
+
+	elif global_position.x > alvo.global_position.x:
+		global_position.x -= speed * delta
+		sprite.flip_h = true
+
+
+func chegou_no_destino():
+	seguindo = false
+	parado_no_alvo = true
+
+
+func voltar_andar():
+	parado_no_alvo = false
+
+
+func movimento_normal(delta):
+	if parado_no_alvo:
+		return
+
+	if !dentro:
+		position.y += speedy * delta
+		speedy += 30
+
+	else:
+		position.x += direction * speed * delta
+
+		if position.x > inicio_x + limite:
+			direction = -1
+			sprite.flip_h = true
+
+		elif position.x < inicio_x - limite:
+			direction = 1
+			sprite.flip_h = false
+
 
 func _process(delta):
 	if seguindo and alvo != null:
+		seguir_alvo(delta)
 
-		if global_position.x < alvo.global_position.x - 5:
-			global_position.x += speed * delta
-			sprite.flip_h = false
+	elif soltou and estoque < capacidade:
+		movimento_normal(delta)
 
-		elif global_position.x > alvo.global_position.x + 5:
-			global_position.x -= speed * delta
-			sprite.flip_h = true
-
-	else:
-		if soltou:
-			if estoque < capacidade:
-
-				# Queda do barco
-				if !dentro:
-					position.y += speedy * delta
-					speedy += 30
-
-				# Movimento normal quando entra na água
-				else:
-					position.x += direction * speed * delta
-
-					if position.x > inicio_x + limite:
-						direction = -1
-						sprite.flip_h = true
-
-					elif position.x < inicio_x - limite:
-						direction = 1
-						sprite.flip_h = false
 
 func _on_timer_timeout() -> void:
 	if estoque < capacidade:
 		estoque += 5
 
+
 func _on_area_2d_area_entered(area: Area2D) -> void:
+
 	if area.name == "mar":
 		dentro = true
+	
+	if seguindo == true:
+		if area.name == "AREA":
+			print("entrou")
+			chegou_no_destino()
+
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
+
 	if area.name == "mar":
 		dentro = false
