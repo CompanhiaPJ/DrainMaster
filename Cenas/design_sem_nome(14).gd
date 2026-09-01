5b0f85323333b35a1ab07edeacf8fb6ee179acaf
WILLIER GONCALVES <CG3038203@car.ifsp>
2026-09-01 08:32:37 -0300

    01.09

--- Cenas/design_sem_nome(14).gd
+++ Cenas/design_sem_nome(14).gd
@@ -17,5 +17,59 @@ func _on_timer_timeout() -> void:
 
 func calendario():
 	if label_numero.text == "31" and label_texto.text == "JAN":
-		label_texto.text = "FEV"
 		numero = 0
+		await get_tree().create_timer(2).timeout
+		label_texto.text = "FEV"
+	
+	if label_numero.text == "28" and label_texto.text == "FEV":
+		numero = 0
+		await get_tree().create_timer(2).timeout
+		label_texto.text = "MAR"
+	
+	if label_numero.text == "31" and label_texto.text == "MAR":
+		numero = 0
+		await get_tree().create_timer(2).timeout
+		label_texto.text = "ABR"
+		
+	if label_numero.text == "30" and label_texto.text == "ABR":
+		numero = 0
+		await get_tree().create_timer(2).timeout
+		label_texto.text = "MAI"
+		
+	if label_numero.text == "31" and label_texto.text == "MAI":
+		numero = 0
+		await get_tree().create_timer(2).timeout
+		label_texto.text = "JUN"
+
+	if label_numero.text == "30" and label_texto.text == "JUN":
+		numero = 0
+		await get_tree().create_timer(2).timeout
+		label_texto.text = "JUL"
+		
+	if label_numero.text == "31" and label_texto.text == "JUL":
+		numero = 0
+		await get_tree().create_timer(2).timeout
+		label_texto.text = "AGO"
+		
+	if label_numero.text == "31" and label_texto.text == "AGO":
+		numero = 0
+		await get_tree().create_timer(2).timeout
+		label_texto.text = "SET"
+		
+	if label_numero.text == "30" and label_texto.text == "SET":
+		numero = 0
+		await get_tree().create_timer(2).timeout
+		label_texto.text = "OUT"
+		
+	if label_numero.text == "31" and label_texto.text == "OUT":
+		numero = 0
+		await get_tree().create_timer(2).timeout
+		label_texto.text = "NOV"
+		
+	if label_numero.text == "30" and label_texto.text == "NOV":
+		numero = 0
+		await get_tree().create_timer(2).timeout
+		label_texto.text = "DEZ"
+		
+	if label_numero.text == "31" and label_texto.text == "DEZ":
+		print("acabou")
